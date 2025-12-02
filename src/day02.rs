2ab8f2947417911fc4b7f84b37f6a804b32dea7c
kaasbroodju <morriswaaijer8@gmail.com>
2025-12-02 23:35:18 +0100

    Refactor: Day02 part 1 number representation

--- src/day02.rs
+++ src/day02.rs
@@ -15,14 +15,30 @@ impl Day<Vec<(usize, usize)>, usize> for Day02 {
         let mut total = 0;
 
         for &(a, b) in input {
-            for i in a..=b {
-                let s = i.to_string();
-                let middle_point = s.len() / 2;
-                let first_half = &s[..middle_point];
-                let second_half = &s[middle_point..];
-                if first_half.eq(second_half) {
-                    total += i;
-                }
+            for n in a..=b {
+
+                // n = "2121"
+                let encoded_number = encode_digits(n);
+                // encoded_number = 0010 0001 0010 0001
+
+                // Tel hoeveel cijfers n heeft
+                let digit_count = (n.ilog10() + 1) as usize;
+
+                // hoeveel bits neem ik mee voor de vergelijking?
+                let half = digit_count / 2;
+                let half_bits = half * 4;
+                let mask = (1u64 << half_bits) - 1;
+                // mask = 0000 0000 1111 1111
+
+                // dmv de mask neem ik de rechter helft alleen mee.
+                let right_half = encoded_number & mask;
+                // right_half = 0010 0001
+
+                // verplaats de linker helft naar de rechterkant en herhaal de vergelijking.
+                let left_half = (encoded_number >> half_bits) & mask;
+
+                // branchless programming: als het aantal cijfers even is en de linker en rechter helft overeenkomen, dan mag i opgeteld worden.
+                total += (digit_count % 2 == 0 && left_half == right_half) as usize * n;
             }
         }
 
@@ -71,4 +87,19 @@ impl Day<Vec<(usize, usize)>, usize> for Day02 {
 
         total
     }
+}
+
+fn encode_digits(mut n: usize) -> u64 {
+    let mut result = 0u64;
+    let mut shift = 0;
+
+    // Extract digits van rechts naar links
+    while n > 0 {
+        let digit = (n % 10) as u64;
+        result |= digit << shift;
+        shift += 4;
+        n /= 10;
+    }
+
+    result
 }
